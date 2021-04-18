0da986c02b12cddb05851438a8223d55309d3de8
Vidhi Mehta <mvidhi40@gmail.com>
2021-04-19 00:08:05 +0530

    19-Apr-2021

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -2,35 +2,58 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText username, password;
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //aa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1 = (Button) findViewById(R.id.button1);
+        btn1 = findViewById(R.id.activity_main_loginButton);
+        username = findViewById(R.id.activity_main_usernameEditText);
+        password = findViewById(R.id.activity_main_passwordEditText);
+        System.out.println(username);
+        System.out.println(password);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)  {
-                Toast.makeText(getBaseContext(), "Toast Message for clicking Login button" , Toast.LENGTH_SHORT ).show();
+                System.out.println("Button Clicked");
+                openNewActivity();
             }
         });
 
-        TextView tv2 = (TextView) findViewById(R.id.textView2);
+        TextView tv2 = findViewById(R.id.textView2);
         tv2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getBaseContext(), "Toast Message for clicking Register Here", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void openNewActivity(){
+        String userName = username.getText().toString();
+        String pass = password.getText().toString();
+        System.out.println(userName);
+        System.out.println(pass);
+        Intent intent;
+        if(userName.equals("admin") && pass.equals("admin")){
+            intent = new Intent(this, SecondActivity.class);
+        }else{
+            Toast.makeText(getBaseContext(), "Wrong credentials. Try again.", Toast.LENGTH_SHORT).show();
+            intent = new Intent(this, MainActivity.class);
+        }
+        startActivity(intent);
+
     }
 }
 
